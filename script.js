d883dadc5c6557a260d437577ba899b60ff24820
ShengtzyS <shengtzy.subaique@genstudents.org>
2024-04-26 13:10:45 -0400

    Tarea html.css.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("Título").innerHTML = "Adiós"
+
+
+document.getElementById("CambioColor").style.color = "orange";
+
+
+function cambiarColorDeFuente(){
+    document.getElementById("Cambiando").style.color = "brown"
+}
